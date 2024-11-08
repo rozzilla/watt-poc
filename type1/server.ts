0f6d5512345dc414713cbe723e96bc7eae27a602
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-11-08 16:24:44 +0100

    Try typescript

---
+++ type1/server.ts
@@ -0,0 +1,15 @@
+import { createServer } from "node:http";
+
+export function build() {
+  let count = 0;
+
+  const server = createServer((req, res) => {
+    console.log("received request2", req.url);
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({ content: `from node3 path "${req.url}": ${count++}!` })
+    );
+  });
+
+  return server;
+}
