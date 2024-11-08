67c61cd9e727fb30253e7c6a7e379dec5677f20a
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-11-08 09:43:03 +0100

    FIx

--- web/fastify2/server.js
+++ web/fastify2/server.js
@@ -1,16 +1,14 @@
 import fastify from "fastify";
 
-export default function () {
-  const app = fastify();
+const app = fastify();
 
-  app.get("/check", async () => {
-    return { status: "ok2" };
-  });
+app.get("/check", async () => {
+  return { status: "ok2" };
+});
 
-  app.listen({ port: 3000 }, (err) => {
-    if (err) {
-      app.log.error(err);
-      process.exit(1);
-    }
-  });
-}
+app.listen({ port: 3000 }, (err) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
