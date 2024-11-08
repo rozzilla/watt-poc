a0c760b11e2b99f5449b6e6a39413bc59b2d7047
Matteo Collina <hello@matteocollina.com>
2024-11-08 15:49:04 +0000

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- type1/server.ts
+++ type1/server.ts
@@ -1,12 +1,14 @@
 import { createServer } from "node:http";
 
-let count = 0;
+export function build() {
+  let count = 0;
 
-const server = createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.end(
-    JSON.stringify({ content: `from type1 path "${req.url}": ${count++}!` })
-  );
-});
+  const server = createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({ content: `from type1 path "${req.url}": ${count++}!` })
+    );
+  });
 
-server.listen(0);
+  return server;
+}
