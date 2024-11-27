b50493e91b747b1fe5925447cabe4963b20b87ea
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-11-27 14:51:25 +0100

    Update

--- bug/index.js
+++ bug/index.js
@@ -1,21 +1,8 @@
 const pino = require("pino");
 
-function buildLoggerFactory() {
-  return pino({
-    transport: {
-      target: "pino/file",
-      level: "error",
-      options: { destination: 1 },
-    },
-  });
-}
-
-const logger = buildLoggerFactory();
+pino({ transport: { target: "pino/file" } });
 
 // Calling the pino builder twice cause "FATAL ERROR: v8::FromJust Maybe value is Nothing"
-buildLoggerFactory();
+pino({ transport: { target: "pino/file" } });
 
-(async () => {
-  logger.error("logging!!!");
-  process.exit(1);
-})();
+(async () => process.exit(1))();
